0444427414e6c503da09f4b83d9816049520b45c
Sertac <sertac@gmail.com>
2022-05-05 22:11:41 -0400

    Added [Serialize Field] and defined our rotatorAxis and rotatorSpeed in a way that is understandable in Unity editor, tweaked the axis and speed values.
    Added [Serialize Field] and defined our rotatorAxis and rotatorSpeed in a way that is understandable in Unity editor, tweaked the axis and speed values.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    [Header("Rotation Controls")]
+
+    [Tooltip("The axis around which the gameObject is rotating")]
+    [SerializeField]
+    private Vector3 rotationAxis = Vector3.up;
+
+    [Tooltip("The speed with which the gameObject is rotating. Measured in degrees/frame")]
+    [SerializeField]
+    private float rotationSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, 1.0f);
+        transform.Rotate(rotationAxis, rotationSpeed);
     }
 }
