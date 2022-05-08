00966101877b66b3f086033894b41a45341f2b8e
Sertac <sertac@gmail.com>
2022-05-07 22:41:09 -0400

    When played collects all 11 pickups, a bonus pickup (prefab) is now instantiated at the last stage.
    When played collects all 11 pickups, a bonus pickup (prefab) is now instantiated at the last stage.

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Bonus : MonoBehaviour
 {
+    //counter for the remaining collectibles in the scene
+    public int counter;
+
+    //The prefab that will be instantiated
+
+    public GameObject bonusObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = GameObject.FindGameObjectsWithTag("Pick Up").Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Pickup()
+    {
+        counter--;
+
+        if (counter == 0)
+            SpawnBonus();
+
+    }
+    void SpawnBonus()
+    {
+        //Instantiate our Prefab!
+        GameObject.Instantiate(bonusObject, transform);
+    }
+
+
+
+
 }
